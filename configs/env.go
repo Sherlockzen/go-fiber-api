9160e90a54d8cf8fa3fd568c784cec821a1a0386
Hewerton <hewertonlourenco@gmail.com>
2023-04-03 23:45:28 -0300

    Bug: try to make railway deploy work

--- configs/env.go
+++ configs/env.go
@@ -3,15 +3,14 @@ package configs
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
-	if err != nil {
+	// err := godotenv.Load()
+	uri := os.Getenv("MONGO_URL")
+	if uri == "" {
 		log.Fatal("Error loading .env file")
 	}
 
-	return os.Getenv("MONGO_URL")
+	return uri
 }
