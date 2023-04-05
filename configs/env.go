2e546c41da4e4ab3d3583cc3a506fe9a0a447017
Hewerton <hewertonlourenco@gmail.com>
2023-04-04 21:24:12 -0300

    Feat: adjusts for working local if the prod variable is false

--- configs/env.go
+++ configs/env.go
@@ -3,10 +3,19 @@ package configs
 import (
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	// err := godotenv.Load()
+	prod := os.Getenv("PROD")
+
+	if prod != "true" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	uri := os.Getenv("MONGO_URL")
 	if uri == "" {
 		log.Fatal("Error loading .env file")
